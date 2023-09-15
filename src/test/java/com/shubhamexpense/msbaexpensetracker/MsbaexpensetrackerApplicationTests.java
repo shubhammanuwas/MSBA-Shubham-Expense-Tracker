ecbeae0ef70de8b45322449125c70ffd9f79a1f5
shubhammanuwas <shubham.manuwas2@gmail.com>
2023-09-15 22:15:54 +0530

    expense files added

---
+++ src/test/java/com/shubhamexpense/msbaexpensetracker/MsbaexpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhamexpense.msbaexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbaexpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
